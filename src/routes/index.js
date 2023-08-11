067361a89d6ec823fb6461fb497260cdcb58b1b7
TheDevMohamed <alkhenaizi.mohamed@gmail.com>
2023-08-11 23:15:08 +0300

    Added watch tower message

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
-  res.send(`<h1>ENV: ${process.env.NODE_ENV} From docker hub! Using watch tower!!</h1>`);
+  res.send(`<h1>ENV: ${process.env.NODE_ENV} From docker hub! Using watch tower!!!</h1>`);
 });
 
 module.exports = router;
