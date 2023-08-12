d8ab863d8e9a898579bf70041e65ffe7d5d8c536
TheDevMohamed <alkhenaizi.mohamed@gmail.com>
2023-08-12 20:00:58 +0300

    Added docker swarm

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 const mongoose = require('mongoose');
+const os = require("os");
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
+  console.log(`traffic from ${os.hostname()}`);
   res.send(`<h1>ENV: ${process.env.NODE_ENV} From docker hub! Using watch tower!!!!</h1>`);
 });
 
